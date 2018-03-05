9ae230b80f87c8272fedfc1c637496cd5f16086e
Cory Eighan <zephyr834@gmail.com>
2018-03-05 14:15:11 -0800

    Part 2: Completed.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -25,15 +25,16 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s[0] =~ /(?i)[a-z &&[^aeiou]]/
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[01]+$/
+  return s.to_i(2)%4 == 0
 end
 
 # Part 3
